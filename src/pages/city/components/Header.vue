65dd8482cfd62751761be16f0c89df2f48e3e029
jambo <602975235@qq.com>
2020-02-28 15:21:33 +0800

    city-header

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header">
+    <router-link to="/">
+      <div class="header-left">
+        <div class="iconfont icon-back"></div>
+      </div>
+    </router-link>
+    城市选择
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    overflow hidden
+    line-height $headerHeight
+    height $headerHeight
+    background $bgColor
+    color: #fff
+    text-align center
+    font-size .32rem
+    .header-left
+      width .64rem
+      float left
+      .icon-back
+        text-align center
+        font-size .4rem
+        color #fff
+</style>
