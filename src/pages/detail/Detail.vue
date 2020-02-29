002e4af4ee757d6cdf6b285173ee4cc27d10c983
jambo <602975235@qq.com>
2020-02-29 17:16:48 +0800

    组件嵌套  解决组件名字由来

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,62 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <detail-list :list="list"></detail-list>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data() {
+    return {
+      list: [
+        {
+          id: '1',
+          title: '成人票',
+          children: [
+            {
+              id: '1',
+              title: '成人三馆联票',
+              children: [
+                {
+                  id: '1',
+                  title: '成人AAA联票'
+                },
+                {
+                  id: '2',
+                  title: '成人BBB联票'
+                }
+              ]
+            },
+            {
+              id: '2',
+              title: '成人五馆联票'
+            }
+          ]
+        },
+        {
+          id: '2',
+          title: '学生票'
+        },
+        {
+          id: '3',
+          title: '儿童票'
+        },
+        {
+          id: '4',
+          title: '特惠票'
+        }
+      ]
+    }
   }
 }
 </script>
