95d040b3293d783d32f0640ce964fb25350cc88c
jambo <602975235@qq.com>
2020-02-29 18:11:21 +0800

    banner animation fade

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -9,12 +9,15 @@
         </div>
       </div>
     </div>
-    <common-gallary :imgs="bannerImgs" v-show="showGarrary" @close="handleGarraryClick"></common-gallary>
+    <fade-animation>
+      <common-gallary :imgs="bannerImgs" v-show="showGarrary" @close="handleGarraryClick"></common-gallary>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'Banner',
   props: {
@@ -36,7 +39,8 @@ export default {
     }
   },
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   }
 }
 </script>
